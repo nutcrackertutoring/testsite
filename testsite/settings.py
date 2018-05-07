8246e9d74a508bdc222ca15fba48fa7969801873
Nathan <nathan.goldwaser@gmail.com>
2018-05-08 09:04:46 +1000

    Allowed hosts

--- testsite/settings.py
+++ testsite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'towy!u-x%q33kj3m--v_3958ima=i*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+ALLOWED_HOSTS = ['vast-anchorage-26244.herokuapp.com','127.0.0.1']
 
 
 # Application definition
